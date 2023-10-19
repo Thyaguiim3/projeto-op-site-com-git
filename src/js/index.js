89a77eec97b68c0e3b236ffeead45514081fb386
Thyaguiim3 <thyaguiim3@gmail.com>
2023-10-19 11:41:38 -0300

    testando git

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const personagens = document.querySelectorAll('.personagem');
+
+
+const personagemSelecionado = document.querySelector ('.selecionado');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado')
+        personagem.classList.add ('selecionado');
+
+        
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+        
+        const idPersonagem = personagem.attributes.id.value;
+        console.log(idPersonagem);
+        imagemPersonagemGrande.src = `./src/imagens/card2-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name')
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description')
+  
+    })
+})
